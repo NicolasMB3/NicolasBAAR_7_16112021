d475c30a496ec389dfca2067e69c118b1b48c91c
Nicolas <76515190+NicolasMB3@users.noreply.github.com>
2021-12-20 03:05:10 +0100

    Fix gutter error (sass)

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -9,55 +9,54 @@ import PageNotFound from "@/components/PageNotFound.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    // Default page
-    path: "/",
-    name: "Login",
-    component: Login,
-  },
-  {
-    // SignUP page
-    path: "/signup",
-    name: "Signup",
-    component: Signup,
-  },
-  {
-    // Get all posts
-    path: "/posts",
-    name: "Posts",
-    component: Posts,
-  },
-  {
-    // Get profil id
-    path: "/profil/:id",
-    name: "Profil",
-    component: Profil,
-  },
-  {
-    // Get all users
-    path: "/users",
-    name: "Users",
-    component: Users,
-  },
-  {
-    // Vue3 required to catchAll *
-    // 404 error PageNotFound
-    path: "/:catchAll(.*)",
-    name: "NotFound",
-    component: PageNotFound,
-    meta: {
-      requiresAuth: false
-    }
-  },
-];
-
 const router = new VueRouter({
-  routes,
+  routes: [
+    {
+      // Default page
+      path: "/",
+      name: "Login",
+      component: Login,
+    },
+    {
+      // SignUP page
+      path: "/signup",
+      name: "Signup",
+      component: Signup,
+    },
+    {
+      // Get all posts
+      path: "/posts",
+      name: "Posts",
+      component: Posts,
+    },
+    {
+      // Get profil id
+      path: "/profil/:id",
+      name: "Profil",
+      component: Profil,
+    },
+    {
+      // Get all users
+      path: "/users",
+      name: "Users",
+      component: Users,
+    },
+    {
+      // Vue3 required to catchAll *
+      // 404 error PageNotFound
+      path: "/:catchAll(.*)",
+      name: "NotFound",
+      component: PageNotFound,
+      meta: {
+        requiresAuth: false
+      }
+    },
+  ]
 });
 
 router.beforeEach((to, from, next) => {
   document.title = to.name
   next()
 });
+
 export default router;
